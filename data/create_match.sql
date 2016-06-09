e2d16962783de0928039985f23530ee724671aea
marduk <dr.marduk@gmail.com>
2016-06-10 01:05:58 +0200

    added the data dir with the sql stuff for the vorrunde and the teams

---
+++ data/create_match.sql
@@ -0,0 +1,43 @@
+insert into match(groupe, name, location, kickoff, host, guest) values
+('A', '1. Vorrunde', 'Saint-Denis', '2016-06-10 21:00:00', 2, 3),
+('A', '1. Vorrunde', 'Lens Agglo', '2016-06-11 15:00:00', 1, 4),
+('A', '2. Vorrunde', 'Paris', '2016-06-15 18:00:00', 3, 4),
+('A', '2. Vorrunde', 'Marseille', '2016-06-15 21:00:00', 2, 1),
+('A', '3. Vorrunde', 'Lille Métropole', '2016-06-19 21:00:00', 4, 2),
+('A', '3. Vorrunde', 'Lyon', '2016-06-19 21:00:00', 3, 1),
+
+('B', '1. Vorrunde', 'Bordeaux', '2016-06-11 18:00:00', 8, 7),
+('B', '1. Vorrunde', 'Marseille', '2016-06-11 21:00:00', 5, 6),
+('B', '2. Vorrunde', 'Lille Métropole', '2016-06-15 15:00:00', 6, 7),
+('B', '2. Vorrunde', 'Lens Agglo', '2016-06-16 15:00:00', 5, 8),
+('B', '3. Vorrunde', 'Saint-Étienne', '2016-06-20 21:00:00', 7, 5),
+('B', '4. Vorrunde', 'Toulouse', '2016-06-20 21:00:00', 6, 8),
+
+('C', '1. Vorrunde', 'Nizza', '2016-06-12 18:00:00', 11, 10),
+('C', '1. Vorrunde', 'Lille Métropole', '2016-06-12 21:00:00', 9, 12),
+('C', '2. Vorrunde', 'Lyon', '2016-06-16 18:00:00', 12, 10),
+('C', '2. Vorrunde', 'Saint-Denis', '2016-06-16 21:00:00', 9, 11),
+('C', '3. Vorrunde', 'Paris', '2016-06-21 18:00:00', 10, 9),
+('C', '4. Vorrunde', 'Marseille', '2016-06-21 18:00:00', 12, 11),
+
+('D', '1. Vorrunde', 'Paris', '2016-06-12 15:00:00', 16, 13),
+('D', '1. Vorrunde', 'Toulouse', '2016-06-13 15:00:00', 15, 14),
+('D', '2. Vorrunde', 'Saint-Étienne', '2016-06-17 18:00:00', 14, 13),
+('D', '2. Vorrunde', 'Nizza', '2016-06-17 21:00:00', 15, 16),
+('D', '3. Vorrunde', 'Bordeaux', '2016-06-21 21:00:00', 13, 15),
+('D', '4. Vorrunde', 'Lens Agglos', '2016-06-21 21:00:00', 14, 16),
+
+('E', '1. Vorrunde', 'Saint-Denis', '2016-06-13 18:00:00', 19, 20),
+('E', '1. Vorrunde', 'Lyon', '2016-06-13 21:00:00', 17, 18),
+('E', '2. Vorrunde', 'Toulouse', '2016-06-17 15:00:00', 18, 20),
+('E', '2. Vorrunde', 'Bordeaux', '2016-06-18 15:00:00', 17, 19),
+('E', '3. Vorrunde', 'Nizza', '2016-06-22 21:00:00', 20, 17),
+('E', '4. Vorrunde', 'Lille Métropole', '2016-06-22 21:00:00', 18, 19),
+
+('F', '1. Vorrunde', 'Bordeaux', '2016-06-14 18:00:00', 21, 22),
+('F', '1. Vorrunde', 'Saint-Étienne', '2016-06-14 21:00:00', 24, 23),
+('F', '2. Vorrunde', 'Marseille', '2016-06-18 18:00:00', 23, 22),
+('F', '2. Vorrunde', 'Paris', '2016-06-18 21:00:00', 24, 21),
+('F', '3. Vorrunde', 'Saint-Denis', '2016-06-22 18:00:00', 23, 21),
+('F', '4. Vorrunde', 'Lyon', '2016-06-22 18:00:00', 22, 24);
+
---
+++ data/create_team.sql
@@ -0,0 +1,4 @@
+Insert into team(country) values('Albanien'), ('Frankreich'), ('Rumänien'),('Schweiz'),('England'),('Russland'),
+('Slowakei'),('Wales'),('Deutschland'),('Nordirland'),('Polen'),('Ukraine'),('Kroatien'),('Tschechische Republik'),
+('Spanien'),('Türkei'),('Belgien'),('Italien'),('Republik Irland'),('Schweden'),('Hitlerreich'),('Ungarn'),
+('Island'),('Portugal');
