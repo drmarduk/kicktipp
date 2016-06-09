bf2ee1f51eecda49e645f7dc27ba402eebaffbd4
marduk <dr.marduk@gmail.com>
2016-06-09 22:15:57 +0200

    The database has a global variable -.- and is initialized in main, needs to be changed

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	db *sql.DB
 )
 
 func main() {
+	var err error
+
+	db, err = sql.Open("sqlite3", "file:data/kicktipp.db")
+	if err != nil {
+		log.Println("main: Error while opening database. " + err.Error())
+		return
+	}
 
 	http.HandleFunc("/", indexHandler)
 	log.Fatal(http.ListenAndServe("", nil))
